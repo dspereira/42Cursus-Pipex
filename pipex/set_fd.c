4a11523eafa13596abc83c192ed1ca7cbc590a63
Diogo Pereira <diogo.pereira.ee@gmail.com>
2022-03-11 18:25:43 +0000

    bug fixed on set_fd.c

--- pipex/set_fd.c
+++ pipex/set_fd.c
@@ -9,13 +9,21 @@ t_fds *set_fd(char *infile, char *outfile, int size)
 
     fds = malloc(sizeof(t_fds));
     fds->fd = malloc(size * sizeof(t_fd));
+    fds->size = size;
+    i = 0;
+    while (i < size)
+    {
+        fds->fd[i].r = -1;
+        fds->fd[i].w = -1;
+        i++;
+    }
     fd = fds->fd;
+    save_alloc_fds(fds);
     fd[0].r = sys_error(open(infile, O_RDONLY));
     fd[0].w = -1;
     fd[size - 1].w = sys_error(open(outfile, O_TRUNC | O_WRONLY));
     fd[size - 1].r = -1;
     i = 1;
-    int pipe_fd1[1];
     while (i < size - 1)
     {
         sys_error(pipe(pipe_fd));
@@ -23,14 +31,14 @@ t_fds *set_fd(char *infile, char *outfile, int size)
 		fd[i].w = pipe_fd[1];
         i++;
     }
-    fds->size = size;
+    //fds->size = size;
     save_alloc_fds(fds);
     return (fds);
 }
 
 void close_fds(t_fds *fds)
 {
-    t_fd *fd;
+    t_fd *fd = 0;
     int i;
 
     if (!fds)
@@ -41,16 +49,17 @@ void close_fds(t_fds *fds)
     {
         if (fd[i].r >= 0)
         {
-            sys_error(close(fd[i].r));
+            close(fd[i].r);
             fd[i].r = -1;
         }
         if (fd[i].w >= 0)
         {
-            sys_error(close(fd[i].w));
+            close(fd[i].w);
             fd[i].w = -1;
         }
         i++;
     }
-    free(fd);
+    if (fd)
+        free(fd);
     free(fds);
 }
