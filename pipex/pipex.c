5d0b4a27fbf1cca09b56179f0425b03b294a7fef
Diogo Pereira <diogo.pereira.ee@gmail.com>
2022-03-15 12:09:34 +0000

    refactored pipex.c

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -1,47 +1,70 @@
 #include "pipex.h"
 
 // gcc pipex.c get_cmd.c get_path.c set_fd.c utils.c utils2.c alloc_mem.c error_handling.c 
-void exec_cmd(t_fds *fds, t_cmd cmd, int i);
+//void	exec_cmd(t_fds *fds, t_cmd cmd, int i);
+//void	exec_cmds(t_fds *fds, t_cmds *cmds, int num_cmds);
 
-int main (int argc, char **argv, char **envp)
+void	exec_cmd(t_fds *fds, t_cmd cmd, int r_fd, int w_fd);
+void	wait_cmds_end(int num_cmds);
+void	exec_cmds(t_fds *fds, t_cmds *cmds, int num_cmds);
+
+int	main(int argc, char **argv, char **envp)
 {
-	int size;
-	t_fds *fds;
-	t_cmds *cmds;
-	t_paths *paths;
-	int i;
-	
-	size = argc - 2;
+	t_fds	*fds;
+	t_cmds	*cmds;
+	t_paths	*paths;
+	int		num_cmds;
+	int		size_arr_fd;
+
+	num_cmds = argc - 3;
+	size_arr_fd = num_cmds + 1;
 	paths = get_path(envp);
-	fds = set_fd(argv[1], argv[argc - 1], size);
-	cmds = get_cmds(argc - 3, (const char**)(argv + 2), (const char **)paths->paths);
-	i = 0;
-	while (i < argc - 3)
-	{
-		exec_cmd(fds, cmds->cmd[i], i);
-		i++;
-	}
+	fds = set_fd(argv[1], argv[argc - 1], size_arr_fd);
+	cmds = get_cmds(num_cmds, (argv + 2), paths->paths);
+	exec_cmds(fds, cmds, num_cmds);
 	close_fds(fds);
-	save_alloc_fds(0);
-	i = 0;
-	int status; 
-	while (i < argc - 3)
-	{
-		wait(&status);
-		i++;
-	}
+	save_alloc_fds(0); // para tirar daqui
+	wait_cmds_end(num_cmds);
 	free_alloc_mem();
 }
 
-void exec_cmd(t_fds *fds, t_cmd cmd, int i)
+void	wait_cmds_end(int num_cmds)
+{	
+	int	i;
+
+	i = 0;
+	while (i < num_cmds)
+	{
+		wait(NULL);
+		i++;
+	}
+}
+
+void	exec_cmds(t_fds *fds, t_cmds *cmds, int num_cmds)
 {
-	int pid;
+	int		i;
+	t_fd	*fd;
+	t_cmd	*cmd;
+
+	fd = fds->fd;
+	cmd = cmds->cmd;
+	i = 0;
+	while (i < num_cmds)
+	{
+		exec_cmd(fds, cmd[i], fd[i].r, fd[i + 1].w);
+		i++;
+	}	
+}
+
+void	exec_cmd(t_fds *fds, t_cmd cmd, int r_fd, int w_fd)
+{
+	int	pid;
 
 	pid = fork();
 	if (!pid)
 	{
-		sys_error(dup2(fds->fd[i].r, STDIN_FILENO));
-		sys_error(dup2(fds->fd[i+1].w, STDOUT_FILENO));
+		sys_error(dup2(r_fd, STDIN_FILENO));
+		sys_error(dup2(w_fd, STDOUT_FILENO));
 		close_fds(fds);
 		if (cmd.cmd[0])
 		{
