ed6a18841b4c69a6558e655b7e4babca306930b8
Diogo Silverio pereira <dsilveri@c1r19s1.42lisboa.com>
2022-04-27 16:06:54 +0100

    removed bonus from Makefile

--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -5,59 +5,54 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dsilveri <dsilveri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/28 12:37:50 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/03/28 12:42:15 by dsilveri         ###   ########.fr       */
+/*   Created: 2022/03/28 12:37:44 by dsilveri          #+#    #+#             */
+/*   Updated: 2022/04/27 15:54:16 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void	exec_cmd(t_fds *fds, t_cmd cmd, t_fd fd, char **env)
+void	exec_cmd(t_fds *fds, t_cmd cmd, int r_fd, int w_fd)
 {
 	int	pid;
 
 	pid = fork();
 	if (!pid)
 	{
-		sys_error(dup2(fd.r, STDIN_FILENO));
-		sys_error(dup2(fd.w, STDOUT_FILENO));
+		sys_error(dup2(r_fd, STDIN_FILENO));
+		sys_error(dup2(w_fd, STDOUT_FILENO));
 		close_fds(fds);
 		if (cmd.cmd[0])
 		{
-			execve(cmd.path, cmd.cmd, env);
+			execve(cmd.path, cmd.cmd, NULL);
 			exit(EXIT_FAILURE);
 		}
 		exit(0);
 	}
 }
 
-void	exec_cmds(t_fds *fds, t_cmds *cmds, char **env)
+void	exec_cmds(t_fds *fds, t_cmds *cmds, int num_cmds)
 {
-	int		n_cmds;
 	int		i;
 	t_fd	*fd;
-	t_fd	actual_fd;
 	t_cmd	*cmd;
 
 	fd = fds->fd;
 	cmd = cmds->cmd;
-	n_cmds = cmds->size;
 	i = 0;
-	while (i < n_cmds)
+	while (i < num_cmds)
 	{
-		actual_fd.r = fd[i].r;
-		actual_fd.w = fd[i + 1].w;
-		exec_cmd(fds, cmd[i], actual_fd, env);
+		exec_cmd(fds, cmd[i], fd[i].r, fd[i + 1].w);
 		i++;
 	}	
 }
 
-void	wait_cmds_end(int n_cmds)
+void	wait_cmds_end(int num_cmds)
 {	
 	int	i;
 
 	i = 0;
-	while (i < n_cmds)
+	while (i < num_cmds)
 	{
 		wait(NULL);
 		i++;
@@ -72,10 +67,10 @@ int	main(int argc, char **argv, char **envp)
 	int		num_cmds;
 	int		size_arr_fd;
 
-	if (argc != 5)
+	if (argc < 5)
 	{
 		ft_printf("%s: Wrong number of parametersn\n", PROGRAM_NAME);
-		ft_printf("Usage \"./pipex infile cmd1 cmd2 outfile\"\n");
+		ft_printf("Usage \"./pipex infile cmd1 cmd2 ... cmdn outfile\"\n");
 		return (0);
 	}
 	num_cmds = argc - 3;
@@ -83,7 +78,7 @@ int	main(int argc, char **argv, char **envp)
 	paths = get_paths(envp);
 	fds = get_fds(argv[1], argv[argc - 1], size_arr_fd);
 	cmds = get_cmds(num_cmds, (argv + 2), paths->paths);
-	exec_cmds(fds, cmds, envp);
+	exec_cmds(fds, cmds, num_cmds);
 	close_fds(fds);
 	wait_cmds_end(num_cmds);
 	free_alloc_mem();
